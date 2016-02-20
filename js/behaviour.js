b3fdacb50e052c9f942910d70ea1d4f16685716f
Jordan Los <Jordan@Mac-mini.local>
2016-02-19 17:18:34 -0700

    Animate Header Carousel and style content text

--- js/behaviour.js
+++ js/behaviour.js
@@ -2,16 +2,7 @@ $( document ).ready(function() {
 
 	var tl = new TimelineLite();
 
-	$('body').click(function(){
-		TweenMax.to('#contact-info', 1, {autoAlpha:1});
-	})
-	$('#nav-bar').click(function(){
-		TweenMax.to('#contact-info', 1, {autoAlpha:0});
-	})
-
-
 	$('#fullpage').fullpage({
-		
 		scrollingSpeed: 1000,
 		easingcss3: 'cubic-bezier(0.230, 1.000, 0.320, 1.000)',
 		
@@ -22,9 +13,24 @@ $( document ).ready(function() {
 				.to('#skewX', 0.25, {left: '100%'}, "open")
 				.to('#logo', 0.5, { transform:'translateY(50%)', top:'0%', }, "open")
 				.to('#logo', 0.5, { marginLeft:'0%', paddingLeft:'16.6666667%', paddingRight:'16.6666667%', width:'100%'})
+				.to('.carousel', 0.5, { top:'12em' })
+				.to('.carousel', 0.5, { height:'20em' })
+				.to('#slogan', 0.5, { autoAlpha:0, display:'none' })
+				.to('.carousel-header_fixed', 0.5, { autoAlpha:1 }, "alpha")
+				.to('.carousel-header_moving', 0.5, { autoAlpha:1}, "alpha")
 				.to('.action-button_floating', 0.5, { bottom:'5em' })
 			} else if (index == 2 && direction == 'down') {
+				tl.to('#carousel-h1', 0.2, { autoAlpha:0 })
+				.to('.carousel-header_moving', 0.3, { top:'-=131px' })
+				.to('#carousel-h2', 0.2, { autoAlpha:1 })
 			} else if (index == 3 && direction == 'down'){
+				tl.to('#carousel-h2', 0.2, { autoAlpha:0 })
+				.to('.carousel-header_moving', 0.3, { top:'-=131px' })
+				.to('#carousel-h3', 0.2, { autoAlpha:1 })
+			} else if (index == 4 && direction == 'down') {
+				tl.to('#carousel-h3', 0.2, { autoAlpha:0 })
+				.to('.carousel-header_moving', 0.3, { top:'-=131px' })
+				.to('#carousel-h4', 0.2, { autoAlpha:1 })
 			}
 			else if (index == 2 && direction == 'up'){
 				tl.reverse()
