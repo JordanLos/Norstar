0792c001e79366ea40a9db52540ff98aee6807fd
Jordan Los <Jordan@Mac-mini.local>
2016-02-26 13:57:47 -0700

    Make reverse scroll show previous carousel text

--- js/behaviour.js
+++ js/behaviour.js
@@ -1,24 +1,29 @@
 $( document ).ready(function() {	
 
 	var tl = new TimelineLite();
-	var lineHeight = $('h1').outerHeight( true );
-	var lineHeightPx = lineHeight + 'px';
-	console.log(lineHeight);
-
 
 	$('#fullpage').fullpage({
 		scrollingSpeed: 1000,
 		easingcss3: 'cubic-bezier(0.230, 1.000, 0.320, 1.000)',
 		
 		'onLeave': function(index, nextIndex, direction) {
+			// Three Scrolling Behaviours
+			// 1. Main Animation to transfer from landing page
+			//	to content
+			// 2. Scroll header text when scrolling down
+			// 3. Scroll header text when scrolling up
 
 			if (index == 1 && direction == 'down') {
+			// 1. Animate landing state to content
+				// A. Unskew and move the colored div
 				tl.to('#skewX', 0.25, {
 					transform: 'skewX(0)'})
 				.to('#skewX', 0.25, {
 					left: '100%' }, 
 					"open" )
 
+				// B. Move the header from center page to the top
+				// of the page and remove the company slogan
 				.to('.header', 0.5, {
 					transform:"translateY(-50%)",
 					top:'0' },
@@ -28,6 +33,8 @@ $( document ).ready(function() {
 					autoAlpha:0 },
 					"scene1")
 				
+				// C. Expand the header to take up the entire width
+				// of the screen and add the carousel text
 				.to(['#carousel1', '.carousel-header'], 0.5, {
 					autoAlpha:1,
 					display:'block' }, 
@@ -38,12 +45,28 @@ $( document ).ready(function() {
 					borderRadius:'0em' },
 					"scene2" )
 
+				// D. Bring the Contact Action buttion into view
 				.to('.action-button_floating', 0.5, { 
 					bottom:'5em' })
+
 			} else if (index > 1 && direction == 'down') {
+				// Identify the current header text to fade out
 				var headerActive = '#carousel' + (index - 1).toString();
+				// Identify the next header text to fade in 
 				var headerNext = '#carousel' + index.toString();
 			
+				// Fade out the carousel text, then remove it from the document flow
+				tl.to(headerActive, 0.3, {
+					autoAlpha:0,
+					display:'none' })
+				// Add the next carousel text to the document flow, the fade in
+				.to(headerNext, 0.3, {
+					display:'block',
+					autoAlpha:1 })
+			} else if (index > 2 && direction == 'up') {
+				var headerActive = '#carousel' + (index - 1).toString();
+				var headerNext = '#carousel' + (index - 2).toString();
+
 				tl.to(headerActive, 0.3, {
 					autoAlpha:0,
 					display:'none' })
